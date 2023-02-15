ef5df646591f97a5f3540f86eedb6b225d94d6dd
Martin Weingart <martinweingart@gmail.com>
2023-02-15 15:31:06 -0300

    Remove default url and set url prop as required

--- src/Footer/Service/index.ts
+++ src/Footer/Service/index.ts
@@ -1,10 +1,7 @@
 import { FooterConfig } from '../Models';
 
-const DEFAULT_URL =
-  'https://smartforceprodcdn.azureedge.us/smartforce/common/footer.json';
-
-export const getConfig = (url?: string): Promise<FooterConfig> => {
-  return fetch(url || DEFAULT_URL, {
+export const getConfig = (url: string): Promise<FooterConfig> => {
+  return fetch(url, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',

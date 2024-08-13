0ebe5ac014dfb738e830c3d1bd4b3822d3c58490
Martin Weingart <martinweingart@gmail.com>
2024-08-13 08:50:30 -0300

    Update text colors to improve contrast for accessibility

--- src/Footer/FooterLink/FooterLink.tsx
+++ src/Footer/FooterLink/FooterLink.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import styles from './FooterLink.module.scss';
 import { Link } from '../Models';
+import { ThemeType } from '../Footer';
 
 export interface FooterLinkProps {
   link: Link;
+  theme?: ThemeType;
 }
 
 export const FooterLink = ({
-  link
+  link,
+  theme
 }: FooterLinkProps): React.ReactElement<FooterLinkProps> => {
   return (
     <a
-      className={styles.footerLink}
+      className={`${styles.footerLink} ${
+        theme === 'night' ? styles.night : ''
+      }`}
       href={link.href}
-      target="_blank"
-      rel="noreferrer"
+      target='_blank'
+      rel='noreferrer'
     >
       {link.label}
     </a>
--- src/Footer/FooterSection/FooterSection.tsx
+++ src/Footer/FooterSection/FooterSection.tsx
@@ -2,20 +2,27 @@ import React from 'react';
 import styles from './FooterSection.module.scss';
 import { FooterLink } from '../FooterLink/FooterLink';
 import { Link, Section } from '../Models';
+import { ThemeType } from '../Footer';
 
 export interface FooterSectionProps {
   section: Section;
+  theme?: ThemeType;
 }
 
 export const FooterSection = ({
-  section
+  section,
+  theme
 }: FooterSectionProps): React.ReactElement<FooterSectionProps> => {
   return (
-    <div className={styles.footerSection}>
+    <div
+      className={`${styles.footerSection} ${
+        theme === 'night' ? styles.night : ''
+      }`}
+    >
       <span className={styles.title}>{section.title}</span>
 
       {section.links.map((link: Link) => (
-        <FooterLink key={link.label} link={link} />
+        <FooterLink key={link.label} link={link} theme={theme} />
       ))}
     </div>
   );
